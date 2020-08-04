4b685bccaafcf1d41b1dfbf2b41de5e40537a896
kunscomputer <kunscomputer@outlook.com>
2020-08-04 16:18:21 +0800

    增加了zookeeper 和 springcloud整合的项目 cloud-consumerzk-order80 和 cloud-provider-payment8003 8003 和80  注册进linux中的zookeeper ,80通过zookeeper调用了8003的方法

---
+++ cloud-consumerzk-order80/src/main/java/com/huashuikun/cloud/controller/OrderZkController.java
@@ -0,0 +1,28 @@
+package com.huashuikun.cloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderZkController {
+
+
+
+    public static final String INVOME_URL = "http://cloud-provider-paymentofzookeeper";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String payment (){
+        String result = restTemplate.getForObject(INVOME_URL+"/payment/zk",String.class);
+        return result;
+    }
+
+}
