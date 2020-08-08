ca6c7fdd226cd88b520e244177a0c47815650ec8
kunscomputer <kunscomputer@outlook.com>
2020-08-08 15:08:45 +0800

    建立的新项目 feign-hystrix-order80 hystrix-payment8001 dashboard9001 测试了熔断机制的使用（通过异常触发服务降级 ，多次异常触发熔断） 用hystrix的监控页面监控服务的访问情况

--- README.md
+++ README.md
@@ -16,3 +16,42 @@ springcloud实例练习项目
 2.ribbon超时配置，在application文件中配置全局ribbion超时时间时，发现idea没有任何提示，无法识别ribbon的这些属性（可能
 因为ribbon是间接导入的？？），但是按照说明配置后，启动项目后发现配置有用
 
+### **项目名：cloud-provider-hystrix-payment8001**
+
+1.在配置hystrix熔断器时，用的@HystrixComment注解，里面有commandProperties这一数组，里面存放的是key value类的属性值，很好奇
+这些属性值从哪里可以看到，通过搜索（shift+shift）发现了HystrixPropertiesCommandDefault extends HystrixCommandProperties,
+下载HystrixCommandProperties的源码 可以看到这些属性值相关注释了解属性是干嘛的
+
+2.熔断器在发生异常时没有进行服务降级操作，发现主启动类没有添加@EnableHystrix （思维导图没有。。。。）
+
+3.在测试时ribbon的时间限制记得关注一下，如果测试的时间过长，ribbion也会引起服务降级，让服务无法成功运行，导致服务永远处于降级状态
+
+### **项目名**：cloud-consumer-hystrix-dashboard9001
+
+1.导入依赖时 ,发现无法导入这个依赖
+
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-netflix-hystrix-dashboard</artifactId>
+</dependency>
+```
+
+这个依赖需要配合两个其他依赖 ，这个时候 ，就发现上面的依赖已经成功导入了
+
+```xml
+<dependency>
+    <groupId>com.netflix.hystrix</groupId>
+    <artifactId>hystrix-javanica</artifactId>
+    <version>RELEASE</version>
+</dependency>
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-netflix-hystrix-dashboard</artifactId>
+</dependency>
+```
+
+2.新版本Hystrix需要在服务端主启动类指定监控路径
+
+3.在访问http://localhost:9001/hystrix/ 监控8001的过程中，页面一直处于loading的空白状态，在网上查询发现，需要在访问和HystrixComment注解相关
+的服务的时候，这个页面才会加载出来！，此时访问http://localhost:8001/payment/circuit/1  画面就出来了
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/huashuikun/cloud/PaymentHystrixMain8001.java
@@ -0,0 +1,37 @@
+package com.huashuikun.cloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+
+    //新版本Hystrix需要在主启动类指定监控路径
+    //主启动类添加代码
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无关,SpringCloud升级后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
+     * 只要在自己的项目里配置上下面的servlet就可以了
+     */
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
